d388494de705b039ef9996723d080060c61afed5
Jigyasa15 <jigyasakodnani21@gmail.com>
2024-10-02 19:02:55 +0530

    add git ignore file

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'ainotify80@gmail.com'
 # os.getenv('MAIL_USERNAME')  # Your email
-app.config['MAIL_PASSWORD'] = 'snyj vlph hbbg ebns'
+app.config['MAIL_PASSWORD'] = os.getenv('GMAIL_PASSWORD')
 # os.getenv('MAIL_PASSWORD')  # Your email password
 app.config['MAIL_DEFAULT_SENDER'] = 'ainotify80@gmail.com'
 mail = Mail(app)
